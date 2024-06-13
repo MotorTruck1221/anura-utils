55103f5bd5b127c707fb17b45f6a519d4936b53d
MotorTruck1221 <motortruck1221@protonmail.com>
2024-06-13 01:41:19 -0600

    Chore: format

--- src/cli.ts
+++ src/cli.ts
@@ -65,136 +65,147 @@ async function project() {
         );
     }
     if (process.env.TERM_PROGRAM?.toLowerCase().includes('mintty')) {
-        prompt.note(chalk.yellow(
+        prompt.note(
+            chalk.yellow(
                 'WARNING: It looks like you are using MinTTY which is not interactive. This is most likely because you are using Git Bash. \nIf you are using Git Bash, please use it from another terminal like Windows Terminal. \nOr use -y/--default to use all default options '
             )
-        )
+        );
         throw new Error();
     }
     const questions = await prompt.group(
         {
             ...(!providedName && {
-                path: () => prompt.text({
-                    message: chalk.blue('Where would you like to create your project?'),
-                    placeholder: 'anura-app'
-                }),
+                path: () =>
+                    prompt.text({
+                        message: chalk.blue('Where would you like to create your project?'),
+                        placeholder: 'anura-app'
+                    })
             }),
             ...(!cliResults.flags.git && {
-                git: () => prompt.confirm({
-                    message: chalk.blueBright('Do you want to create a Git repository?'),
-                    initialValue: false
-                }),
+                git: () =>
+                    prompt.confirm({
+                        message: chalk.blueBright('Do you want to create a Git repository?'),
+                        initialValue: false
+                    })
             }),
             ...(!cliResults.flags.license && {
-                license: () => prompt.text({
-                    message: chalk.cyan('What is the license you are going to use?'),
-                    placeholder: 'MIT'
-                }),
+                license: () =>
+                    prompt.text({
+                        message: chalk.cyan('What is the license you are going to use?'),
+                        placeholder: 'MIT'
+                    })
             }),
             ...(!cliResults.flags.author && {
-                author: () => prompt.text({
-                    message: chalk.cyanBright('What is your name?'),
-                    placeholder: 'Billy'
-                }),
+                author: () =>
+                    prompt.text({
+                        message: chalk.cyanBright('What is your name?'),
+                        placeholder: 'Billy'
+                    })
             }),
             ...(!cliResults.flags.projectType && {
-                projType: () => prompt.select({
-                    message: chalk.magenta('What project type do you want to use?'),
-                    initialValue: 'ts',
-                    maxItems: 2,
-                    options: [
-                        { value: 'ts', label: chalk.bold.blue('TS') },
-                        { value: 'js', label: chalk.bgYellow.bold.black('JS') },
-                        //Uncomment when supported: { value: 'assemblyscript', label: 'AssemblyScript' }
-                    ],
-                }),
+                projType: () =>
+                    prompt.select({
+                        message: chalk.magenta('What project type do you want to use?'),
+                        initialValue: 'ts',
+                        maxItems: 2,
+                        options: [
+                            { value: 'ts', label: chalk.bold.blue('TS') },
+                            { value: 'js', label: chalk.bgYellow.bold.black('JS') }
+                            //Uncomment when supported: { value: 'assemblyscript', label: 'AssemblyScript' }
+                        ]
+                    })
             }),
             ...(!cliResults.flags.install && {
-                install: () => prompt.confirm({
-                    message: chalk.magentaBright('Do you want to automatically install dependencies?'),
-                    initialValue: false 
-                }),
+                install: () =>
+                    prompt.confirm({
+                        message: chalk.magentaBright(
+                            'Do you want to automatically install dependencies?'
+                        ),
+                        initialValue: false
+                    })
             }),
             ...(!cliResults.flags.dreamland && {
-                dreamland: () => prompt.confirm({
-                    message: chalk.green('Do you want to use dreamland.js?'),
-                    initialValue: false,
-                }),
+                dreamland: () =>
+                    prompt.confirm({
+                        message: chalk.green('Do you want to use dreamland.js?'),
+                        initialValue: false
+                    })
             })
         },
-        { 
+        {
             onCancel: () => {
                 prompt.cancel(chalk.red.bold('Operation canceled'));
                 process.exit(0);
             }
-        })
-        let promptToContinue;
-        if (cliResults.flags.dreamland && cliResults.flags.projectType === "js") {
-            prompt.note(chalk.yellow("When using JavaScript dreamland.js is not supported."));
-            promptToContinue = await prompt.group({
-               continue: () => prompt.confirm({
-                   message: chalk.bold.yellow("Do you want to continue?"),
-                   initialValue: true,
-               })
+        }
+    );
+    let promptToContinue;
+    if (cliResults.flags.dreamland && cliResults.flags.projectType === 'js') {
+        prompt.note(chalk.yellow('When using JavaScript dreamland.js is not supported.'));
+        promptToContinue = await prompt.group(
+            {
+                continue: () =>
+                    prompt.confirm({
+                        message: chalk.bold.yellow('Do you want to continue?'),
+                        initialValue: true
+                    })
             },
             {
                 onCancel: () => {
-                    prompt.cancel("Process Canceled");
+                    prompt.cancel('Process Canceled');
                     process.exit(0);
                 }
-            })
-            if (!promptToContinue.continue) {
-                prompt.cancel(chalk.red("Ok, canceling"));
-                process.exit(0);
             }
-            else {
-                prompt.note(chalk.green("Ok, ignoring dreamland option"));
-            }
+        );
+        if (!promptToContinue.continue) {
+            prompt.cancel(chalk.red('Ok, canceling'));
+            process.exit(0);
+        } else {
+            prompt.note(chalk.green('Ok, ignoring dreamland option'));
         }
-        else if (questions.dreamland && questions.projType === "js") {
-            prompt.note(chalk.yellow("When using JS, dreamland.js is not supported"));
-            promptToContinue = await prompt.group({
-                continue: () => prompt.confirm({
-                    message: chalk.yellow.bold("Do you want to continue?"),
-                    initialValue: true,
-                }),
+    } else if (questions.dreamland && questions.projType === 'js') {
+        prompt.note(chalk.yellow('When using JS, dreamland.js is not supported'));
+        promptToContinue = await prompt.group(
+            {
+                continue: () =>
+                    prompt.confirm({
+                        message: chalk.yellow.bold('Do you want to continue?'),
+                        initialValue: true
+                    })
             },
             {
                 onCancel: () => {
-                    prompt.cancel(chalk.red("Process canceled"));
+                    prompt.cancel(chalk.red('Process canceled'));
                     process.exit(0);
                 }
-            });
-            if (!promptToContinue.continue) {
-                prompt.cancel(chalk.red("Ok, canceling"));
-                process.exit(0)
             }
-            else {
-                prompt.note(chalk.green("Ok, ignoring dreamland selection"));
-            }
+        );
+        if (!promptToContinue.continue) {
+            prompt.cancel(chalk.red('Ok, canceling'));
+            process.exit(0);
+        } else {
+            prompt.note(chalk.green('Ok, ignoring dreamland selection'));
         }
-        const spinner = prompt.spinner();
-        spinner.start();
-        spinner.message(chalk.yellow("Scaffolding project"))
-        let dreamland;
-        if (promptToContinue?.continue) {
-            dreamland = false;
-        }
-        else if (cliResults.flags.dreamland) {
-            dreamland = true;
-        }
-        else if (questions.dreamland) {
-            dreamland = true;
-        }
-        else {
-            dreamland = false;
-        }
-        await scaffold({
-            projectName: questions.path ?? cliResults.dir,
-            type: questions.projType ?? cliResults.flags.projectType,
-            dreamland: dreamland
-        })
-        spinner.stop();
+    }
+    const spinner = prompt.spinner();
+    spinner.start();
+    spinner.message(chalk.yellow('Scaffolding project'));
+    let dreamland;
+    if (promptToContinue?.continue) {
+        dreamland = false;
+    } else if (cliResults.flags.dreamland) {
+        dreamland = true;
+    } else if (questions.dreamland) {
+        dreamland = true;
+    } else {
+        dreamland = false;
+    }
+    await scaffold({
+        projectName: questions.path ?? cliResults.dir,
+        type: questions.projType ?? cliResults.flags.projectType,
+        dreamland: dreamland
+    });
+    spinner.stop();
 }
 
 async function cli() {
--- src/scaffold.ts
+++ src/scaffold.ts
@@ -1,22 +1,21 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import chalk from 'chalk';
 import fs from 'fs-extra';
 import { downloadTemplate } from 'giget';
 import sortPackageJson from 'sort-package-json';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 interface options {
-    projectName: string,
-    type: string,
-    dreamland: boolean,
+    projectName: string;
+    type: string;
+    dreamland: boolean;
 }
 
 async function template(template: string, name: string) {
-    try {}
-    catch {}
+    try {
+    } catch {}
 }
 
-async function scaffold(opts: options) {
-}
+async function scaffold(opts: options) {}
 
 export { scaffold };
