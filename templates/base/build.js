2dfabf7b69847746464e3bdd738f49df0b53e389
MotorTruck1221 <motortruck1221@protonmail.com>
2024-08-29 03:44:26 -0600

    start the work on the templates

---
+++ templates/base/build.js
@@ -0,0 +1,29 @@
+import fs from 'node:fs';
+import archiver from 'archiver';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import pc from 'picocolors';
+const manifest = JSON.parse(fs.readFileSync(path.join(path.dirname(fileURLToPath(import.meta.url)), "src/manifest.json")));
+
+const output = fs.createWriteStream(path.join(path.dirname(fileURLToPath(import.meta.url)), `${manifest.package}.app.zip`));
+const archive = archiver('zip', {
+    zlib: { level: 0 }
+});
+
+output.on("close", function () {
+    console.log(pc.green(`Finished building app package: ${manifest.package}`))
+    console.log(pc.yellow(`${archive.pointer()} total bytes`));
+});
+
+archive.on("warning", function (err) {
+    if (err.code === "ENOENT") { console.warn(pc.yellow(err)); }
+    else { throw err }
+});
+
+archive.on("error", function (err) {
+    throw err;
+});
+
+archive.pipe(output);
+archive.directory(path.join(path.dirname(fileURLToPath(import.meta.url)), "src"), false);
+archive.finalize();
