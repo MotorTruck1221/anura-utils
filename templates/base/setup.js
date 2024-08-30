856712e69a9eebf3bad8bcfb3614d50090913bbc
MotorTruck1221 <motortruck1221@protonmail.com>
2024-08-30 03:11:02 -0600

    GUH

--- templates/base/setup.js
+++ templates/base/setup.js
@@ -1,6 +1,21 @@
 //This file exists solely to symlink your app to the anura_env/ folder.
-//It is most likely not required to re-run it. (and it can be deleted).
-import { symlinkSync } from "node:fs";
+//It is most likely not required to re-run it. (and it is auto deleted).
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const packageJSON = fs.readJSONSync(path.join(path.dirname(fileURLToPath(import.meta.url)), "package.json"));
+const manifest = JSON.parse(fs.readFileSync(path.join(path.dirname(fileURLToPath(import.meta.url)), "src/manifest.json")));
 
-console.log("Symlink setting up...");
-symlinkSync("./src/", './anura_env/', 'dir');
+console.log("Creating symlink...");
+fs.symlink(path.join(path.dirname(fileURLToPath(import.meta.url)), "src"), path.join(path.dirname(fileURLToPath(import.meta.url)), `anura_env/${manifest.package}.app`), 'dir', (err) => {
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.log("Symlink created!");
+        packageJSON.scripts.postinstall = "";
+        fs.writeJSONSync(path.join(path.dirname(fileURLToPath(import.meta.url)), "package.json"), packageJSON, {
+            spaces: 2
+        });
+    }
+});
