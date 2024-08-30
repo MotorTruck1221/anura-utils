19d088a580f0311dcaf9bf524a0ec84288963162
MotorTruck1221 <motortruck1221@protonmail.com>
2024-08-30 03:20:32 -0600

    Include wisp in the server

--- templates/js/server.js
+++ templates/js/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import wisp from 'wisp-server-node';
 
 app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "/anura_env/public/")));
 app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "/anura_env/build/")));
