67e592a4e277abef84623af38d304d05b3d50241
MotorTruck1221 <motortruck1221@protonmail.com>
2024-09-01 01:33:01 -0600

    Start work on the TS template

--- create-anura-app/templates/base/build.js
+++ create-anura-app/templates/base/build.js
@@ -3,34 +3,36 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import archiver from 'archiver';
 import pc from 'picocolors';
-const manifest = JSON.parse(
-    fs.readFileSync(path.join(path.dirname(fileURLToPath(import.meta.url)), 'src/manifest.json'))
-);
+function build(ts) {
+    const manifest = JSON.parse(
+        fs.readFileSync(path.join(path.dirname(fileURLToPath(import.meta.url)), 'src/manifest.json'))
+    );
 
-const output = fs.createWriteStream(
-    path.join(path.dirname(fileURLToPath(import.meta.url)), `${manifest.package}.app.zip`)
-);
-const archive = archiver('zip', {
-    zlib: { level: 0 }
-});
+    const output = fs.createWriteStream(
+        path.join(path.dirname(fileURLToPath(import.meta.url)), `${manifest.package}.app.zip`)
+    );
+    const archive = archiver('zip', {
+        zlib: { level: 0 }
+    });
 
-output.on('close', function () {
-    console.log(pc.green(`Finished building app package: ${manifest.package}`));
-    console.log(pc.yellow(`${archive.pointer()} total bytes`));
-});
+    output.on('close', function () {
+        console.log(pc.green(`Finished building app package: ${manifest.package}`));
+        console.log(pc.yellow(`${archive.pointer()} total bytes`));
+    });
 
-archive.on('warning', function (err) {
-    if (err.code === 'ENOENT') {
-        console.warn(pc.yellow(err));
-    } else {
+    archive.on('warning', function (err) {
+        if (err.code === 'ENOENT') {
+            console.warn(pc.yellow(err));
+        } else {
+            throw err;
+        }
+    });
+
+    archive.on('error', function (err) {
         throw err;
-    }
-});
+    });
 
-archive.on('error', function (err) {
-    throw err;
-});
-
-archive.pipe(output);
-archive.directory(path.join(path.dirname(fileURLToPath(import.meta.url)), 'src'), false);
-archive.finalize();
+    archive.pipe(output);
+    ts === true ? archive.directory(path.join(path.dirname(fileURLToPath(import.meta.url)), 'src', 'dist'), false) : archive.directory(path.join(path.dirname(fileURLToPath(import.meta.url)), 'src'), false);
+    archive.finalize();
+}
