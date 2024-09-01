907461d45acdfdfcff9077befb8b85fc69c71464
MotorTruck1221 <motortruck1221@protonmail.com>
2024-09-01 02:51:51 -0600

    Sooo close...

--- create-anura-app/templates/ts/server.ts
+++ create-anura-app/templates/ts/server.ts
@@ -6,10 +6,10 @@ import { fileURLToPath } from 'node:url';
 import wisp from 'wisp-server-node';
 import { Socket } from 'node:net';
 
-app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "/anura_env/public/")));
-app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "/anura_env/build/")));
-app.use("/__mnt", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "dist/src/")));
-app.use("/apps", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "/anura_env/apps/")));
+app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "../anura_env/public/")));
+app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "../anura_env/build/")));
+app.use("/__mnt", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "src/")));
+app.use("/apps", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "../anura_env/apps/")));
 
 const server = app.listen(port, () => console.log("Listening on port: ", port))
 server.on("upgrade", (request: any, socket: any, head: any) => {
